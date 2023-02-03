ea0ee1a43c22063a933cb2c8b05fab70f2703bf7
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2023-02-03 15:42:09 +0700

    lesson complete

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,10 +1,12 @@
 import React, { useRef } from 'react';
 
 // import useHistory here.
+import { useHistory } from 'react-router-dom';
 
 const Search = () => {
 
   // get the history object here
+  const history = useHistory();
 
   const searchInputRef = useRef();
 
@@ -16,6 +18,7 @@ const Search = () => {
     }).toString();
 
     // imperatively redirect with history.push()
+    history.push('/search?' + searchQuery)
   };
 
   return (
